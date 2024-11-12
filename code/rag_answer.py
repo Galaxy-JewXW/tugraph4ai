444bba4843e1dc78ac0e3037e94ea7fba18fd4d7
pigKiller <22373017@buaa.edu.cn>
2024-11-12 19:02:33 +0800

    在得到答案后进行一次概括

--- code/rag_answer.py
+++ code/rag_answer.py
@@ -4,6 +4,41 @@ import json
 from rag import *
 from setting import *
 
+def _get_llm() :
+    from llm.ChatOpenAI import get_ChatOpenAI
+    llm = get_ChatOpenAI()
+
+    return llm
+
+def conclude(question, answer, verbose=False):
+    PROMPT_TEMPLATE = """
+    对问题的答案进行概括，给出一个更简要的答案。
+    如果你觉得原始答案已经足够简洁，也可以不进行概括。
+    问题是：
+    {question}
+    ---
+    答案是：
+    {answer}
+    ---
+    直接给出概括后的答案
+    """
+
+    prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+
+    chat_llm_chain = LLMChain(
+        llm= _get_llm(),
+        prompt=prompt,
+        verbose=verbose, # 是否显示Token
+    )
+
+    # 生成回复
+    res = chat_llm_chain.predict(
+        question=question,
+        answer=answer
+    )
+
+    return res
+
 def _answer_problem(problem_file, result_file):
     rag = RAG()
 
@@ -14,12 +49,16 @@ def _answer_problem(problem_file, result_file):
             question = data["input_field"]
 
             result = rag.search(question)
+            result_conclude = conclude(question, result)
             results.append({
                 "id": data["id"],
-                "output_field": result
+                "output_field": result_conclude
             })
 
-            print(f"[answer]: \n\tquestion is: {question}: \n\tanswer   is: {result}")
+            print(f"[answer]: \n"
+                  f"\tquestion is: {question}: \n"
+                  f"\tanswer   is: {result}\n"
+                  f"\tconclude is: {result_conclude}\n")
             print("##################################\n\n")
 
     # 将提取的数据写入到answer.jsonl文件中
